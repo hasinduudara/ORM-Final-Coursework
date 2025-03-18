66c9267bb415132128e013955c7fcca25aa8c445
hasinduudara <hasindutechschool@gmail.com>
2025-03-18 13:37:51 +0530

    create admin log page

---
+++ src/main/java/com/serenity/hospital/ormcoursework/controller/AdminLogPageViewController.java
@@ -0,0 +1,29 @@
+package com.serenity.hospital.ormcoursework.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+
+public class AdminLogPageViewController {
+
+    @FXML
+    private AnchorPane adminLogPage;
+
+    @FXML
+    private Button btnAdminGoAccCreatePage;
+
+    @FXML
+    private Button btnAdminGoLogPage;
+
+    @FXML
+    void btnAdminGoAccCreatePageOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnAdminGoLogPageOnAction(ActionEvent event) {
+
+    }
+
+}
