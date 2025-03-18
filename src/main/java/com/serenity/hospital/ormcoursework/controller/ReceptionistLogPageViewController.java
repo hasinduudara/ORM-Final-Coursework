2de3750ec761d592d68d1ed4127a3ce43d68d7e1
hasinduudara <hasindutechschool@gmail.com>
2025-03-18 19:56:37 +0530

    create receptionist log page

---
+++ src/main/java/com/serenity/hospital/ormcoursework/controller/ReceptionistLogPageViewController.java
@@ -0,0 +1,29 @@
+package com.serenity.hospital.ormcoursework.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+
+public class ReceptionistLogPageViewController {
+
+    @FXML
+    private Button btnReceptionistGoAccCreatePage;
+
+    @FXML
+    private Button btnReceptionistGoLogPage;
+
+    @FXML
+    private AnchorPane receptionistLogPage;
+
+    @FXML
+    void btnReceptionistGoAccCreatePageOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnReceptionistGoLogPageOnAction(ActionEvent event) {
+
+    }
+
+}
