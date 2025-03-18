41da9d7e47a426bdb042f0906c889a2e50c87ba1
hasinduudara <hasindutechschool@gmail.com>
2025-03-18 10:28:10 +0530

    add dependency

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,18 @@
 module com.serenity.hospital.ormcoursework {
     requires javafx.controls;
     requires javafx.fxml;
+    requires lombok;
+    requires com.jfoenix;
+    requires net.sf.jasperreports.core;
+    requires java.mail;
+
+    requires org.hibernate.orm.core;
+    requires jakarta.persistence;
+    requires java.naming;
+
+    opens com.serenity.hospital.ormcoursework.config to jakarta.persistence;
+    opens com.serenity.hospital.ormcoursework.entity to org.hibernate.orm.core;
+    opens com.serenity.hospital.ormcoursework.dto.tm to javafx.base;
 
 
     opens com.serenity.hospital.ormcoursework to javafx.fxml;
