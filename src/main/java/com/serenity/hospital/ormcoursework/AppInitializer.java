9b9340f57f1377abc1e5c4366b79a8784a5dc0b5
hasinduudara <hasindutechschool@gmail.com>
2025-03-18 12:07:10 +0530

    update welcome page

--- src/main/java/com/serenity/hospital/ormcoursework/AppInitializer.java
+++ src/main/java/com/serenity/hospital/ormcoursework/AppInitializer.java
@@ -1,4 +1,29 @@
 package com.serenity.hospital.ormcoursework;
 
-public class AppInitializer {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class AppInitializer extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent load = FXMLLoader.load(getClass().getResource("/view/WelcomePageView.fxml"));
+
+
+        stage.setTitle("The Serenity Mental Health Therapy Center");
+
+        Scene scene = new Scene(new Group(load));
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("/assets/images/mental-health-icon.png")));
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
--- src/main/java/com/serenity/hospital/ormcoursework/controller/WelcomePageController.java
+++ src/main/java/com/serenity/hospital/ormcoursework/controller/WelcomePageController.java
@@ -1,4 +1,29 @@
 package com.serenity.hospital.ormcoursework.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+
 public class WelcomePageController {
+
+    @FXML
+    private Button btnAdmin;
+
+    @FXML
+    private Button btnReceptionist;
+
+    @FXML
+    private AnchorPane welcomePage;
+
+    @FXML
+    void btnAdminOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnReceptionistOnAction(ActionEvent event) {
+
+    }
+
 }
