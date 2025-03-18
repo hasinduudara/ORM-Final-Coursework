cd01c74e8b8951caf2fbe3d7c4fdd58cd85ddc63
hasinduudara <hasindutechschool@gmail.com>
2025-03-18 23:17:40 +0530

    update

--- src/main/java/com/serenity/hospital/ormcoursework/controller/ReceptionistLogPageViewController.java
+++ src/main/java/com/serenity/hospital/ormcoursework/controller/ReceptionistLogPageViewController.java
@@ -3,6 +3,8 @@ package com.serenity.hospital.ormcoursework.controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
 public class ReceptionistLogPageViewController {
@@ -11,10 +13,19 @@ public class ReceptionistLogPageViewController {
     private Button btnReceptionistGoAccCreatePage;
 
     @FXML
-    private Button btnReceptionistGoLogPage;
+    private Button btnReceptionistLogForgetPass;
+
+    @FXML
+    private Button btnReceptionistSignIn;
+
+    @FXML
+    private PasswordField psReceptionistLogPassword;
 
     @FXML
     private AnchorPane receptionistLogPage;
+
+    @FXML
+    private TextField txtReceptionistLogUserName;
 
     @FXML
     void btnReceptionistGoAccCreatePageOnAction(ActionEvent event) {
@@ -22,7 +33,12 @@ public class ReceptionistLogPageViewController {
     }
 
     @FXML
-    void btnReceptionistGoLogPageOnAction(ActionEvent event) {
+    void btnReceptionistLogForgetPassOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnReceptionistSignInOnAction(ActionEvent event) {
 
     }
 
