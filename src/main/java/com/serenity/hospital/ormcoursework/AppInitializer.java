86a5797334788dc2f4e86dd4579929723d41b1b0
hasinduudara <hasindutechschool@gmail.com>
2025-03-19 12:22:09 +0530

    add icon

--- src/main/java/com/serenity/hospital/ormcoursework/AppInitializer.java
+++ src/main/java/com/serenity/hospital/ormcoursework/AppInitializer.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class AppInitializer extends Application {
@@ -11,7 +12,9 @@ public class AppInitializer extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/WelcomePageView.fxml"));
+        primaryStage.setTitle("The Serenity Mental Health Therapy Center");
         Parent root = loader.load();
+        primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/assets/icon-helth.png")));
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);
         primaryStage.show();
