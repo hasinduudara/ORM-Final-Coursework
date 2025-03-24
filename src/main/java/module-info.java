e215bb86e3662f26be411b0c5bf282a2f1c32f0f
hasinduudara <hasindutechschool@gmail.com>
2025-03-24 10:10:05 +0530

    create hibernate file and factory configuration

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,18 @@ module com.serenity.hospital.ormcoursework {
     requires javafx.fxml;
     requires javafx.graphics;
     requires java.sql;
+    requires lombok;
+    requires com.jfoenix;
+    requires net.sf.jasperreports.core;
 
+    requires org.hibernate.orm.core;
+    requires java.naming;
+    requires jakarta.persistence;
+
+    opens com.serenity.hospital.ormcoursework.config to jakarta.persistence;
+    opens com.serenity.hospital.ormcoursework.entity to org.hibernate.orm.core;
     opens com.serenity.hospital.ormcoursework.controller to javafx.fxml;
+    opens com.serenity.hospital.ormcoursework.dto to javafx.base;
+
     exports com.serenity.hospital.ormcoursework;
 }
