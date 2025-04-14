0852d64c31723afb0730830f73a54514a9aab4aa
hasinduudara <hasindutechschool@gmail.com>
2025-04-14 19:47:58 +0530

    Patient management update

--- src/main/java/com/serenity/hospital/ormcoursework/dao/PatientDAO.java
+++ src/main/java/com/serenity/hospital/ormcoursework/dao/PatientDAO.java
@@ -7,13 +7,5 @@ import java.util.List;
 public interface PatientDAO extends CrudDAO<Patient> {
     Patient getById(String patientId);
 
-//    boolean save(Patient entity);
-//
-//    boolean update(Patient entity);
-//
-//    boolean delete(String patientId);
-//
-//    String getNextId();
-
     List<Patient> getAll();
 }
